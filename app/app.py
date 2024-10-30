504d9fa4712fb71770dcd11c6ad216ae058a08bf
leoferamos <fernandesramosleonardo@gmail.com>
2024-10-30 20:55:58 -0300

    Alteração de teste de deploy

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Bem vindo ao mundo Cloud!🚀'
+    return 'Bem vindo ao mundo Cloud!🚀☁️'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
