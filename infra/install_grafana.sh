b029e8901cb51adcfe68f2d9b102743b5285d55d
Leonardo Ramos <131378510+leoferamos@users.noreply.github.com>
2024-10-31 14:17:59 -0300

    Alteração na instalação

--- infra/install_grafana.sh
+++ infra/install_grafana.sh
@@ -2,24 +2,12 @@
 
 # Adicionar repositórios
 echo "Adicionando repositórios do Helm..."
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 
 # Criar o namespace "monitoring"
 echo "Criando o namespace 'monitoring'..."
 kubectl create namespace monitoring || echo "Namespace 'monitoring' já existe."
-
-# Instalar o Prometheus
-echo "Instalando Prometheus..."
-helm install prometheus prometheus-community/kube-prometheus-stack --namespace monitoring
-
-# Verificar se o Prometheus foi implantado
-echo "Aguardando os pods do Prometheus ficarem prontos..."
-kubectl wait --namespace monitoring \
-  --for=condition=ready pod \
-  -l "app.kubernetes.io/name=kube-prometheus-stack" \
-  --timeout=300s
 
 # Instalar o Grafana
 echo "Instalando Grafana..."
