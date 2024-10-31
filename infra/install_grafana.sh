7164509bd73584fc0717c23ea8efbee8b4c185ee
Leonardo Ramos <131378510+leoferamos@users.noreply.github.com>
2024-10-31 13:01:59 -0300

    Informar como acessar o Grafana

--- infra/install_grafana.sh
+++ infra/install_grafana.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # Adicionar repositórios
+echo "Adicionando repositórios do Helm..."
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 
 # Criar o namespace "monitoring"
-kubectl create namespace monitoring
+echo "Criando o namespace 'monitoring'..."
+kubectl create namespace monitoring || echo "Namespace 'monitoring' já existe."
 
 # Instalar o Prometheus
 echo "Instalando Prometheus..."
@@ -30,4 +32,9 @@ kubectl wait --namespace monitoring \
   -l "app.kubernetes.io/name=grafana" \
   --timeout=300s
 
-echo "Prometheus e Grafana foram instalados com sucesso."
+# Informar como acessar o Grafana
+echo "Grafana instalado com sucesso!"
+echo "Para acessar o Grafana, você pode usar o seguinte comando para fazer o port-forward:"
+echo "kubectl --namespace monitoring port-forward svc/grafana 3000:80"
+echo "Acesse Grafana em http://localhost:3000 com o usuário padrão 'admin' e a senha gerada aleatoriamente. Você pode obtê-la com o seguinte comando:"
+echo "kubectl get secret --namespace monitoring grafana -o jsonpath='{.data.admin-password}' | base64 --decode"
