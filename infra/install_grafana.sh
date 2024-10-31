1142d1462be8bd6c97fd95ad5a577d722260633c
Leonardo Ramos <131378510+leoferamos@users.noreply.github.com>
2024-10-31 13:01:09 -0300

    Corrigindo Instalação

--- infra/install_grafana.sh
+++ infra/install_grafana.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
 
-# Instalação do Prometheus e Grafana
-echo "Installing Prometheus and Grafana..."
-kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/main/bundle.yaml
- 
-echo "Prometheus and Grafana installation complete."
+# Adicionar repositórios
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+
+# Criar o namespace "monitoring"
+kubectl create namespace monitoring
+
+# Instalar o Prometheus
+echo "Instalando Prometheus..."
+helm install prometheus prometheus-community/kube-prometheus-stack --namespace monitoring
+
+# Verificar se o Prometheus foi implantado
+echo "Aguardando os pods do Prometheus ficarem prontos..."
+kubectl wait --namespace monitoring \
+  --for=condition=ready pod \
+  -l "app.kubernetes.io/name=kube-prometheus-stack" \
+  --timeout=300s
+
+# Instalar o Grafana
+echo "Instalando Grafana..."
+helm install grafana grafana/grafana --namespace monitoring
+
+# Verificar se o Grafana foi implantado
+echo "Aguardando os pods do Grafana ficarem prontos..."
+kubectl wait --namespace monitoring \
+  --for=condition=ready pod \
+  -l "app.kubernetes.io/name=grafana" \
+  --timeout=300s
+
+echo "Prometheus e Grafana foram instalados com sucesso."
