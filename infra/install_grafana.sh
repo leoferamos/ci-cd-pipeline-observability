6f6267f46e7d147a4b0c64e5b393974b1a7fc09d
Leonardo Ramos <131378510+leoferamos@users.noreply.github.com>
2024-11-01 11:08:42 -0300

    Corrigindo script de instalação

--- infra/install_grafana.sh
+++ infra/install_grafana.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Adicionar repositórios
+# Adicionar repositórios do Helm
 echo "Adicionando repositórios do Helm..."
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
@@ -10,13 +10,20 @@ helm repo update
 echo "Criando o namespace 'monitoring'..."
 kubectl create namespace monitoring || echo "Namespace 'monitoring' já existe."
 
-# Instalar o Prometheus
+# Instalar Prometheus
 echo "Instalando Prometheus..."
 helm install prometheus prometheus-community/prometheus --namespace monitoring
 
-# Instalar o Grafana
+# Instalar Grafana
 echo "Instalando Grafana..."
 helm install grafana grafana/grafana --namespace monitoring
+
+# Verificar se o Prometheus foi implantado
+echo "Aguardando os pods do Prometheus ficarem prontos..."
+kubectl wait --namespace monitoring \
+  --for=condition=ready pod \
+  -l "app.kubernetes.io/name=prometheus" \
+  --timeout=300s
 
 # Verificar se o Grafana foi implantado
 echo "Aguardando os pods do Grafana ficarem prontos..."
@@ -29,5 +36,9 @@ kubectl wait --namespace monitoring \
 echo "Grafana instalado com sucesso!"
 echo "Para acessar o Grafana, você pode usar o seguinte comando para fazer o port-forward:"
 echo "kubectl --namespace monitoring port-forward svc/grafana 3000:80"
-echo "Acesse Grafana em http://localhost:3000 com o usuário padrão 'admin' e a senha gerada aleatoriamente. Você pode obtê-la com o seguinte comando:"
+echo "Acesse Grafana em http://localhost:3000 com o usuário padrão 'admin' e a senha gerada aleatoriamente."
+echo "Você pode obtê-la com o seguinte comando:"
 echo "kubectl get secret --namespace monitoring grafana -o jsonpath='{.data.admin-password}' | base64 --decode"
+
+# Informar que o Prometheus foi instalado
+echo "Prometheus instalado com sucesso! Acesse o Prometheus em http://localhost:9090"
